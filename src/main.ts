5b31e719548be02d5cb1551eb1af7453a78c21f0
patil-sandhya <begadesandhya@gmail.com>
2025-06-20 16:13:54 +0530

    resolve cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://mortgagepro-nine.vercel.app', 
+    origin: ['http://localhost:3000', 'https://mortgagepro-nine.vercel.app'], 
     credentials: true,              
   });
 
